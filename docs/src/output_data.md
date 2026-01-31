a57af0f0b5252c9b8f7a856501fd052b9a46c501
DESKTOP-FCMGFG7\ewi <ewi@wip.tu-berlin.de>
2026-01-31 13:25:42 +0100

    changed documentation headers for data input and output

--- docs/src/output_data.md
+++ docs/src/output_data.md
@@ -1,5 +1,5 @@
 # Model Data
-## Output Data Load
+## Load Data Output
 Model outputs are stored in .arrow files. These files are non-human-readable, but are significantly faster to process compared to other formats like CSV or XLXS. To further process model results, they can be read-in by calling the DataFiles constructor
 ```@docs
 DataFiles
--- src/solving.jl
+++ src/solving.jl
@@ -176,7 +176,6 @@ function _run(mr::ModelRun{ZonalMarket{FlowBased}, PS, RD}) where {PS<:NoProsume
         TwoDayAhead_results = prev_results_for_fbmc(sr)
         # Calculate FBMC parameters from TwoDayAhead basecase
         fbmc_params = calc_fbmc_params(sr,mr.params, TwoDayAhead_results)
-        @show fbmc_params
         # Zonal flow-based market optimization
         ProgressMeter.update!(prog, desc = "DayAhead -> Building Model")
         market_state = DayAhead(T, fbmc_params)
